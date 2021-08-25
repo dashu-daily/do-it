1be4664c132befb4613f9eb4ef91850b59be916e
DESKTOP-BF4LI9P\A <dashu.daily@gmail.com>
2021-08-25 19:56:38 +0900

    add Max3m.java, Q1~3.java

---
+++ src/chap01/Q3.java
@@ -0,0 +1,15 @@
+package chap01;
+
+public class Q3 {
+	static int min4(int a, int b, int c, int d) {
+		int min = a;
+		if(b < min) min = b;
+		if(c < min) min = c;
+		if(d < min) min = d;
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("min4(4, 3, 2, 1) = " + min4(4, 3, 2, 1));
+	}
+}
